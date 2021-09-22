fd123c974063e1ee7b7a101c8a0656d5210a5272
manujacobt <manujacobt@gmail.com>
2021-09-22 10:21:31 +0530

    4th

--- js/login.js
+++ js/login.js
@@ -1,45 +1,64 @@
 let user=document.getElementById("user")
 let pwd=document.getElementById("pwd")
+var flag=0;
 
-login(sub);
- function login(callback){    
+function log(){
+  
+ function login(a,b,callback){    
+   console.log(a);
+   console.log(b);
    
-    if((user.value.trim()!="")&&(user.value!="admin")){
-      alert("check username");
-     
-    }
     
-    
-    else if(user.value=="admin") {
+    if(user.value==a) {
       
-      callback();
-  }
-  else{
-    return false;
-  }
+      callback(a,b);
+      return false;
+   }
+   else{
+     return false;
+   }
       
   }
   
-  function sub(){
-    console.log();
-    if((pwd.value.trim()!="")&&(pwd.value!="12345")){
-      alert("check  pwd");
-      return false;
-    }
-    else
-     if (pwd.value=="12345"){
-     return true;
+  function sub(c,d){
+    console.log(c);
+    console.log(d);
+   
+    
+     if (pwd.value==d){
+     flag=1;
     }
     else{
       return false;
     }
     
   }
+  
 
+  login("admin","12345",sub);
+  
+  if(flag==1){
+    return true;
+  }
 
+  
+  else if((user.value.trim()!="")&&(user.value!="admin")){
+    alert("check username");
+  return false;
+  
+}   
 
+  else if((pwd.value.trim()!="")&&(pwd.value!="12345")){
+    alert("check  pwd");
+    return false;
+  }
+  else{
+    return false;
+  }
+
+  
  
-
+}
 
 
 
